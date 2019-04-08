467b69d5b50c2cb9e9428e2e5561323fc8481653
Abhinav Anand <abhinav_anand@live.com>
2019-04-08 22:50:57 +0530

    Read dummy csv file

--- src/main.cpp
+++ src/main.cpp
@@ -1,8 +1,20 @@
-#include<iostream>
+#include <iostream>
+#include "util.h"
 
 using namespace std;
 
-int main() {
-    cout << "hello world";
+int main(int argc, char **argv) {
+    string filepath;
+    if (argc != 2) {
+        // filepath not supplied in command line args
+        // STOPSHIP
+        filepath = "/home/abhinav/projects/ipsc-proj/data/train.csv";
+//        return -1;
+    } else {
+        filepath = argv[1];
+    }
+
+    auto df = read_csv(filepath);
+    cout << "dataframe :-\n" << df << endl;
     return 0;
 }
